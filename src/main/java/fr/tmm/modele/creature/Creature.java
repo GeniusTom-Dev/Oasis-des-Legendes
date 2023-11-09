f0cf077d28f76e89b36eb37a12795d973ec2d8ab
maxime <maxime.jauras@etu.univ-amu.fr>
2023-11-09 10:52:16 +0000

    Ajout méthode manger et dormir.

--- src/main/java/fr/tmm/modele/creature/Creature.java
+++ src/main/java/fr/tmm/modele/creature/Creature.java
@@ -7,9 +7,11 @@ public abstract class Creature {
     private double weight;
     private double height;
     private int age;
-    private boolean hunger;
-    private boolean sleep;
-    private boolean health;
+    private int hunger;
+    private int sleep;
+    private int health;
+    private static final int HUNGER_MAX = 100;
+    private static final int SLEEP_MAX = 100;
 
     public Creature(String name, String sex, double weight, double height, int age) {
         this.name = name;
@@ -17,9 +19,9 @@ public abstract class Creature {
         this.weight = weight;
         this.height = height;
         this.age = age;
-        this.hunger = true;
-        this.sleep = false;
-        this.health = true;
+        this.hunger = hunger;
+        this.sleep = sleep;
+        this.health = health;
         this.type = this.getClass().getSimpleName();
     }
 
@@ -63,27 +65,73 @@ public abstract class Creature {
         this.age = age;
     }
 
-    public boolean isHunger() {
+    public int getHunger() {
         return hunger;
     }
 
-    public void setHunger(boolean hunger) {
+    public void setHunger(int hunger) {
         this.hunger = hunger;
     }
 
-    public boolean isSleep() {
+    public int getSleep() {
         return sleep;
     }
 
-    public void setSleep(boolean sleep) {
+    public void setSleep(int sleep) {
         this.sleep = sleep;
     }
 
-    public boolean isHealth() {
+    public int getHealth() {
         return health;
     }
 
-    public void setHealth(boolean health) {
+    public void setHealth(int health) {
         this.health = health;
+    }
+
+    public void hungerLevel() {
+        if (hunger >= 0 && hunger <= 25) {
+            System.out.println(getName() + " a une faim intense.");
+            // Logique spécifique pour une faim intense
+        } else if (hunger > 25 && hunger <= 50) {
+            System.out.println(getName() + " a une faim importante.");
+            // Logique spécifique pour une faim importante
+        } else if (hunger > 50 && hunger <= 75) {
+            System.out.println(getName() + " a une faim modérée.");
+            // Logique spécifique pour une faim modéréeé
+        } else if (hunger > 75 && hunger <= 100) {
+            System.out.println(getName() + " a une faim légère.");
+            // Logique spécifique pour une faim légère
+        } else {
+            System.out.println("La valeur de la faim n'est pas dans une plage valide.");
+            // Logique par défaut pour les valeurs de faim en dehors des plages définies
+        }
+    }
+
+    public void eat() {
+        setHunger(HUNGER_MAX);
+    }
+
+    public void sleepLevel() {
+        if (sleep >= 0 && sleep <= 25) {
+            System.out.println(getName() + " a un sommeil intense.");
+            // Logique spécifique pour un sommeil intense
+        } else if (sleep > 25 && sleep <= 50) {
+            System.out.println(getName() + " a un sommeil important.");
+            // Logique spécifique pour un sommeil important
+        } else if (sleep > 50 && sleep <= 75) {
+            System.out.println(getName() + " a un sommeil modéré.");
+            // Logique spécifique pour un sommeil modéré
+        } else if (sleep > 75 && sleep <= 100) {
+            System.out.println(getName() + " a un sommeil léger.");
+            // Logique spécifique pour un sommeil léger
+        } else {
+            System.out.println("La valeur de sommeil n'est pas dans une plage valide.");
+            // Logique par défaut pour les valeurs de sommeil en dehors des plages définies
+        }
+    }
+
+    public void sleep() {
+        setHunger(SLEEP_MAX);
     }
 }
