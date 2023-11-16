7acfaf278e2315d4c067c060bf06191b9bd2d7c0
EVEN-Tom-2225033a <tom.even@etu.univ-amu.fr>
2023-11-16 16:25:47 +0100

    avancement

--- src/main/java/fr/tmm/App.java
+++ src/main/java/fr/tmm/App.java
@@ -7,6 +7,8 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 /**
  * La classe principale de l'application.
  */
@@ -32,13 +34,39 @@ public class App extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
+        appStage = stage;
         stage.setResizable(false);
         stage.setTitle("Oasis des légendes");
-        root = FXMLLoader.load(App.class.getClassLoader().getResource("layout/home.fxml"));
-        Image icon = new Image(getClass().getClassLoader().getResourceAsStream("assets/logo.png"));
+        root = FXMLLoader.load(Objects.requireNonNull(App.class.getClassLoader().getResource("layout/home.fxml")));
+        Image icon = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("assets/logo.png")));
         stage.getIcons().add(icon);
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    /**
+     * Définit la scène sur le fichier FXML spécifié et renvoie le contrôleur associé.
+     *
+     * @param fxmlPath Le chemin vers le fichier FXML.
+     * @param <T>      Le type du contrôleur.
+     * @return Le contrôleur associé au fichier FXML.
+     * @author Even Tom
+     */
+    public static <T> T setScene(String fxmlPath, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(App.class.getClassLoader().getResource(fxmlPath));
+            root = loader.load();
+            appStage.setScene(new Scene(root));
+            if(Objects.equals(title, "")){
+                appStage.setTitle("Oasis des légendes");
+            }else{
+                appStage.setTitle("Oasis des légendes | " + title);
+            }
+            return loader.getController();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
--- src/main/java/fr/tmm/controlers/HomeController.java
+++ src/main/java/fr/tmm/controlers/HomeController.java
@@ -1,4 +1,27 @@
 package fr.tmm.controlers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+import static fr.tmm.App.setScene;
+
+/**
+ * Classe HomeController : Contrôleur de la vue home.fxml.
+ * Permet de gérer les interactions et les fonctionnalités de la vue Home.
+ * @Version 1.0
+ * @author Even Tom
+ */
 public class HomeController {
+
+    @FXML
+    private BorderPane borderPane;
+
+
+    @FXML
+    private void toEnclosure() {
+        EnclosureController enclosureController = (EnclosureController) setScene("layout/enclosure.fxml", "Enclos");
+        assert enclosureController != null;
+        //send data if needed
+    }
+
 }
