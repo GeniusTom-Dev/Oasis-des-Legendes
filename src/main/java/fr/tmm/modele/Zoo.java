b7da3068642e2c16b52bdf58787e8749f8b2866c
Mathieu Leroux <113862047+Badlix@users.noreply.github.com>
2023-11-17 10:03:53 +0100

    Update Zoo.java

--- src/main/java/fr/tmm/modele/Zoo.java
+++ src/main/java/fr/tmm/modele/Zoo.java
@@ -7,18 +7,49 @@ import java.util.ArrayList;
 
 public class Zoo {
 
-    public ArrayList<Enclosure> getEnclosure() {
-        return enclosure;
+    private String name;
+    private ZooMaster zooMaster;
+    final private int NB_MAX_ENCLOSURE;
+    private ArrayList<Enclosure> enclosures;
+
+    // NAME
+
+    public String getName() {
+        return this.name;
     }
 
-    public void setEnclosure(ArrayList<Enclosure> enclosure) {
-        this.enclosure = enclosure;
+    public void setName(String newName) {
+        this.name = newName;
+    }   
+
+    // ZOO MASTER
+
+    public ZooMaster getZooMaster() {
+        return this.zooMaster;
     }
 
-    public ArrayList<Enclosure> enclosure;
+    // NB MAX ENCLOSURE
 
-    public ArrayList<Creature> getAllCreatures() {
-        return new ArrayList<>();
+    public int getNbMaxEnclosure() {
+        return this.NB_MAX_ENCLOSURE;
+    }
+
+    // Enclosures
+
+    public ArrayList<Enclosure> getEnclosures() {
+        return this.enclosures;
+    }
+    
+    public void setEnclosures(ArrayList<Enclosure> enclosures) {
+        this.enclosures = enclosures;
+    }
+
+    public void addAnEnclosure(Enclosure enclosure) {
+        if (this.enclosures.size() < NB_MAX_ENCLOSURE) {
+            this.enclosures.add(enclosure);
+            return 0;
+        }
+        return -1;
     }
 
 }
