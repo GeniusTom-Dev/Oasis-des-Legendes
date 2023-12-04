d14f17e8a8dc5d7cc3e9a288ed853154a9009da7
Badlix <mathieu.leroux173@gmail.com>
2023-12-04 12:10:57 +0100

    add function to determine a lycanthrope level

--- src/main/java/fr/tmm/modele/creature/species/Lycanthrope.java
+++ src/main/java/fr/tmm/modele/creature/species/Lycanthrope.java
@@ -2,6 +2,7 @@ package fr.tmm.modele.creature.species;
 
 import fr.tmm.modele.creature.reproduction.Viviparous;
 import fr.tmm.modele.creature.methodOfMovement.Walker;
+import fr.tmm.modele.lycanthropeColony.Colony;
 import fr.tmm.modele.lycanthropeColony.Pack;
 
 public class Lycanthrope extends Viviparous implements Walker {
@@ -10,9 +11,9 @@ public class Lycanthrope extends Viviparous implements Walker {
     private enum AgeCategorie {
         JEUNE, ADULTE, VIEUX;
     }
-    private int force;
-    private int dominationFactor;
-    private String rang; // alpha, beta, ..., omega
+    private int force; // nb de 0 à 10
+    private int dominationFactor; // nb de gens dominé sur nb de gens qui domine
+    private Character rank; // alpha, beta, ..., omega
     private int level; // ageCategorie + force + dominationFactor + rang
     private int impetuosityFactor;
     private Pack pack;
@@ -35,6 +36,17 @@ public class Lycanthrope extends Viviparous implements Walker {
 
     public void transform() {
         // TODO -> humain/loup -> surement hyper casse couille à faire
+    }
+
+    public void calculLevel() {
+        int tmpLevel = 0;
+        if (this.ageCategorie == AgeCategorie.JEUNE) tmpLevel += 2;
+        else if (this.ageCategorie == AgeCategorie.ADULTE) tmpLevel += 7;
+        else if (this.ageCategorie == AgeCategorie.VIEUX) tmpLevel += 5;
+        tmpLevel += this.force;
+        tmpLevel += Colony.allRank.size() - Colony.allRank.indexOf(this.rank);
+        tmpLevel += this.dominationFactor;
+        this.level = tmpLevel;
     }
 
     @Override
--- src/main/java/fr/tmm/modele/lycanthropeColony/Colony.java
+++ src/main/java/fr/tmm/modele/lycanthropeColony/Colony.java
@@ -3,9 +3,12 @@ package fr.tmm.modele.lycanthropeColony;
 import fr.tmm.modele.creature.species.Lycanthrope;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Colony {
-
     private ArrayList<Pack> packs;
     private ArrayList<Lycanthrope> loneWolf;
+    public static final List<Character> allRank = List.of('α', 'β', 'γ', 'δ', 'ε', 'ω');
 }
